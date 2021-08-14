08cf211aec2f73cdd23487145f2b9f54c939aa21
Pranav <pranavruparel714@gmail.com>
2021-08-15 00:16:04 +0530

    dashboard made, India and statewise, date objects yet to be modified

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask, render_template
-
+from datetime import date
+from flask import Flask, render_template, request
+from data import newData, dataIndia
 app = Flask(__name__)
 
+# gets today's date
+today = date.today().strftime("%Y-%m-%d")  
 
+# index route
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+#login route
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+# page to handle get requests for statewise data
+@app.route('/handler/state/')
+def stateData():
+    # state will be passed as parameter    
+    state = request.args.get('state')
+    # call newData function, gets stateData from data store
+    return newData(today, state)
+
+@app.route('/handler/India')
+def indiaData(): 
+    # gets data for India, on load of app
+    #change here to get last refreshed date
+    return dataIndia("2021-08-14")
+    
 
 if __name__ == '__main__':
     app.run()
--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,4 @@
+/*
 function GetTodayDate() {
     var tdate = new Date();
     var dd = tdate.getDate(); //yields day
@@ -6,16 +7,61 @@ function GetTodayDate() {
     var currentDate = yyyy + "-" + (MM + 1) + "-" + dd;
 
     return currentDate;
-}
+}*/
+/*
+ * $.get("https://api.rootnet.in/covid19-in/stats/history", function (data, status) {
+        console.log(status);
+        var data_today = JSON.stringify(data["data"][data["data"].length - 1]);
+        console.log(data_today);
+    });
+    $.get("https://api.rootnet.in/covid19-in/stats/latest", function (data, status) {
+        console.log(status);
+        console.log(JSON.stringify(data))
+    })
+ */
 
 
 
 
 $(document).ready(function () {
-    var date = GetTodayDate();
-    console.log(date);
-    $.get("https://api.rootnet.in/covid19-in/stats/history", function (data, status) {
-        console.log(status);
-        console.log(JSON.stringify(data))
-    });
+    // send states to populate dropdown
+    var states = ['Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh', 'Dadra and Nagar Haveli and Daman and Diu', 'Delhi', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerala', 'Ladakh', 'Lakshadweep', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttarakhand', 'Uttar Pradesh', 'West Bengal']
+    for(let i = 0; i < states.length; i++){
+        $("#state-select").append(`<option value="`+states[i]+`">`+states[i]+`</option>`)
+    }
+
+    // get India's data on page ready
+    $.ajax({
+        url:'/handler/India'
+    }).done(response => {
+        // contains current day's data
+        console.log(response)
+        $.get("https://api.rootnet.in/covid19-in/stats/latest", function (data) {
+        
+        console.log(data); // contains all time data
+        // update htmls
+        $("#total-cases").html(data.data.summary.total)
+            $("#total-deaths").html(data.data.summary.deaths)
+            $("#new-deaths").html(response.newDeaths)
+            $("#new-discharged").html(response.newDischarged)
+            $('#total-discharged').html(data.data.summary.discharged)
+            $("#new-cases").html(response.newCases)
+    })
+    })
+    // on selection of a particular state, send AJAX request to fetch data for that state
+    $("#state-select").on("change", function(){
+        $.ajax({
+            url: "/handler/state?state=" + $("#state-select").val()
+        }).done(response=>{
+            // make html changes
+            $("#total-cases").html(response.totalConfirmed)
+            $("#total-deaths").html(response.deaths)
+            $("#new-deaths").html(response.newDeaths)
+            $("#new-discharged").html(response.newDischarged)
+            $('#total-discharged').html(response.discharged)
+            $("#new-cases").html(response.newCases)
+            $('#selected-state').html($("#state-select").val())
+        })
+    })
+    
 })
